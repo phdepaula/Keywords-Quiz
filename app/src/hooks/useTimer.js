d4debe68751588eec9fe17c1a34e8ce3f7077b28
Pedro Henrique de Paula Alves <pedrohenriquedepaulaalves@yahoo.com.br>
2025-01-23 22:43:45 -0300

    feat: Including hook useTimer

---
+++ app/src/hooks/useTimer.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+
+export default function useTimer({
+    activate,
+    timeRemaining,
+    setTimeRemaining,
+    action,
+}) {
+    useEffect(() => {
+        if (timeRemaining <= 0) {
+            action();
+        } else if (activate) {
+            const timeInterval = setInterval(() => {
+                setTimeRemaining(timeRemaining - 1);
+            }, 1000);
+
+            return () => clearInterval(timeInterval);
+        }
+    }, [activate, timeRemaining]);
+}
