0fae5455ddbabc76844675c7708499dae83b9039
Pedro Henrique de Paula Alves <pedrohenriquedepaulaalves@yahoo.com.br>
2025-01-25 16:31:54 -0300

    feat: Addind status state

--- app/src/components/Keyword.js
+++ app/src/components/Keyword.js
@@ -1,9 +1,16 @@
+import { useEffect, useState } from 'react';
+
 export default function Keyword(props) {
     const { keyword, inputValue } = props;
+    const [status, setStatus] = useState(false);
+
+    useEffect(() => {
+        if (!status && keyword === inputValue) {
+            setStatus(true);
+        }
+    }, [inputValue]);
 
     return (
-        <div className="item-view">
-            <span>{keyword}</span>
-        </div>
+        <div className="item-view">{status ? <span>{keyword}</span> : ''}</div>
     );
 }
