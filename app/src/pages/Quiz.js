b8ee32958d6fc61c72545972606ba3d94b51aff5
Pedro Henrique de Paula Alves <pedrohenriquedepaulaalves@yahoo.com.br>
2025-01-25 15:01:01 -0300

    feat: Including UserInptu into Page

--- app/src/pages/Quiz.js
+++ app/src/pages/Quiz.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import Button from '../components/button';
 import Counters from '../components/counters';
+import UserInput from '../components/UserInput';
 import useTimer from '../hooks/useTimer';
+import { apiData } from '../settings';
 
 import '../styles/quiz.css';
 
@@ -10,6 +12,18 @@ export default function Quiz() {
     const [score, setScore] = useState(0);
     const [timeRemaining, setTimeRemaining] = useState(5 * 60);
     const maxScore = 50;
+    const [inputValue, setInputValue] = useState('');
+    const [results, setResults] = useState([]);
+
+    const checkInputValue = (value) => {
+        if (apiData.includes(value)) {
+            setResults((prevValues) => [...prevValues, value]);
+            setInputValue('');
+            setScore(score + 1);
+        } else {
+            setInputValue(value);
+        }
+    };
 
     useTimer(activate, timeRemaining, setTimeRemaining, () =>
         setActivate(false),
@@ -18,22 +32,32 @@ export default function Quiz() {
     return (
         <div className="page">
             <div className="page-content">
-                <div className="header">
-                    <div className="header-button">
-                        <Button
-                            text="PLAY QUIZ"
-                            onClick={() => setActivate(true)}
-                        />
-                    </div>
-                    <div className="header-counters">
-                        <Counters
-                            name="SCORE"
-                            indicator={`${score}/${maxScore}`}
-                        />
-                        <Counters
-                            name="TIMER"
-                            indicator={`${Math.floor(timeRemaining / 60)}:${(timeRemaining % 60).toString().padStart(2, '0')}`}
-                        />
+                <div className="quiz">
+                    <div className="header">
+                        <div className="header-left">
+                            {activate ? (
+                                <UserInput
+                                    description="Enter keyword:"
+                                    value={inputValue}
+                                    action={checkInputValue}
+                                />
+                            ) : (
+                                <Button
+                                    text="PLAY QUIZ"
+                                    onClick={() => setActivate(true)}
+                                />
+                            )}
+                        </div>
+                        <div className="header-counters">
+                            <Counters
+                                name="SCORE"
+                                indicator={`${score}/${maxScore}`}
+                            />
+                            <Counters
+                                name="TIMER"
+                                indicator={`${Math.floor(timeRemaining / 60)}:${(timeRemaining % 60).toString().padStart(2, '0')}`}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
