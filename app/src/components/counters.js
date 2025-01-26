4bd8b16c9cd2119079facc6be7b8a53e848f73b3
Pedro Henrique de Paula Alves <pedrohenriquedepaulaalves@yahoo.com.br>
2025-01-26 10:58:01 -0300

    feat: Adding hidden selection

--- app/src/components/counters.js
+++ app/src/components/counters.js
@@ -1,14 +1,32 @@
+import { useState } from 'react';
+
 import arrow from '../assets/arrow.svg';
+import HiddenSelection from './HiddeSelection';
+
 import '../styles/counters.css';
 
 export default function Counters(props) {
-    const { name, indicator, action } = props;
+    const { name, indicator } = props;
+    const [selectionActivated, setSelectionActivated] = useState(false);
 
     return (
         <div className="counters-container">
-            <div className="counters-container-options" onClick={action}>
-                <span>{name}</span>
-                <img alt="arrow" src={arrow} />
+            <div className="counters-options-container">
+                <div
+                    className="counters-container-options"
+                    onClick={() => setSelectionActivated(true)}
+                >
+                    <span>{name}</span>
+                    <img alt="arrow" src={arrow} />
+                </div>
+                {selectionActivated ? (
+                    <HiddenSelection
+                        options={['Numerical', 'Percentage']}
+                        value=""
+                    />
+                ) : (
+                    ''
+                )}
             </div>
             <div className="counters-container-indicator">
                 <span>{indicator}</span>
